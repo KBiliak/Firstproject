19ef14909b5c40ae5bac441d304a7f6b97a19bda
Kate Biliak <kate.bilyak@gmail.com>
2021-11-11 13:58:39 +0200

    update dev branch: uncomment program

--- src/program/FirstProgram.java
+++ src/program/FirstProgram.java
@@ -1,30 +1,30 @@
-package program;
+//package program;
 
-public class FirstProgram {
-
-
-    public static void calc(int a, int b, char operator){
-        switch (operator) {
-            case '+':
-                System.out.println(a + b);
-                break;
-            case '-':
-                System.out.println(a - b);
-                break;
-            case '*':
-                System.out.println(a * b);
-                break;
-            case '/':
-                System.out.println(a / b);
-                break;
-            default:
-                System.out.println("Что-то пошло не так, повторите заново");
-                break;
-        }
-    }
+//public class FirstProgram {
 
 
-    public static void main(String[] args) {
-        calc(4, 2, '/');
-    }
-}
+ //   public static void calc(int a, int b, char operator){
+ //       switch (operator) {
+ //           case '+':
+  //              System.out.println(a + b);
+ //               break;
+ //           case '-':
+ //               System.out.println(a - b);
+  //              break;
+   //         case '*':
+  //              System.out.println(a * b);
+ //               break;
+ //           case '/':
+  //              System.out.println(a / b);
+  //              break;
+  //          default:
+     //           System.out.println("Что-то пошло не так, повторите заново");
+//                break;
+   //     }
+ //   }
+
+
+  //  public static void main(String[] args) {
+  //      calc(4, 2, '/');
+   // }
+//}
